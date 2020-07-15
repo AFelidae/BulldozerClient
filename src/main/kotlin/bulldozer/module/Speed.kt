6b103270536e8e26b2514072bfaf3e94f6724273
Afel <prteza@gmail.com>
2020-07-15 00:37:48 -0700

    Updated speed just to make it simpler
    Lets you jump whenever you want and gives you maximum speed potential with this method

--- src/main/kotlin/bulldozer/module/Speed.kt
+++ src/main/kotlin/bulldozer/module/Speed.kt
@@ -11,15 +11,13 @@ import com.google.common.eventbus.Subscribe
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket
 
-class Speed: Module("Speed", arrayOf(SettingDouble("Height",0.1, 0.0,1.0))) {
+class Speed: Module("Speed", emptyArray<Any>()) {
     @Subscribe
     fun onTick(event: Tick) {
         if(!toggled || mc.options.keyJump.isPressed || !mc.player!!.isSprinting) return
         if(mc.player!!.isOnGround) {
             mc.player!!.jump()
-            mc.player!!.updatePosition(mc.player!!.x, mc.player!!.y + (settings[0] as SettingDouble).value, mc.player!!.z)
             mc.player!!.setVelocity(mc.player!!.velocity.x, 0.0, mc.player!!.velocity.z)
         }
-
     }
 }
