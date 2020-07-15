946b5a1ef492b22d3b55bfa10aeb0329db144ec2
Afel <prteza@gmail.com>
2020-07-15 00:06:01 -0700

    Added adjustable height for speed

--- src/main/kotlin/bulldozer/module/Speed.kt
+++ src/main/kotlin/bulldozer/module/Speed.kt
@@ -5,18 +5,19 @@ import bulldozer.events.ClientMove
 import bulldozer.events.SendPacket
 import bulldozer.events.Tick
 import bulldozer.gui.SettingBoolean
+import bulldozer.gui.SettingDouble
 import bulldozer.gui.SettingFloat
 import com.google.common.eventbus.Subscribe
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket
 
-class Speed: Module("Speed", emptyArray()) {
+class Speed: Module("Speed", arrayOf(SettingDouble("Height",0.1, 0.0,1.0))) {
     @Subscribe
     fun onTick(event: Tick) {
         if(!toggled || mc.options.keyJump.isPressed || !mc.player!!.isSprinting) return
         if(mc.player!!.isOnGround) {
             mc.player!!.jump()
-            mc.player!!.updatePosition(mc.player!!.x, mc.player!!.y + 0.1, mc.player!!.z)
+            mc.player!!.updatePosition(mc.player!!.x, mc.player!!.y + (settings[0] as SettingDouble).value, mc.player!!.z)
             mc.player!!.setVelocity(mc.player!!.velocity.x, 0.0, mc.player!!.velocity.z)
         }
 
