7c5fa36faf28eb0c92005f42b74defc2f1ed078c
Afel <prteza@gmail.com>
2020-08-29 20:37:52 -0700

    Made mouse sens scale with zoom

--- src/main/kotlin/bulldozer/module/Zoom.kt
+++ src/main/kotlin/bulldozer/module/Zoom.kt
@@ -5,13 +5,16 @@ import bulldozer.gui.SettingFloat
 
 class Zoom: Module("Zoom", arrayOf(SettingFloat("zoom", 2.0f, 1.0f, 64.0f))) {
     open var level: Float = 1.0f
-
+    open var sens: Double = 0.0
 
     override fun onEnable(){
+        sens = mc.options.mouseSensitivity
         level = (settings[0] as SettingFloat).value
+        mc.options.mouseSensitivity = mc.options.mouseSensitivity / level
     }
 
     override fun onDisable(){
         level = 1.0f;
+        mc.options.mouseSensitivity = sens
     }
 }
