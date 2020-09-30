e5d631bdfe088832a52a33d950f08295d2ffc3af
Afel <prteza@gmail.com>
2020-09-30 00:55:29 -0700

    headdesk module added
    makes you look down like a spin bot.

---
+++ src/main/kotlin/bulldozer/module/Headdesk.kt
@@ -0,0 +1,27 @@
+package bulldozer.module
+
+import bulldozer.Module
+import bulldozer.events.ReadPacket
+import bulldozer.events.Tick
+import com.google.common.eventbus.Subscribe
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket.*;
+
+class Headdesk: Module("Headdesk", emptyArray<Any>()) {
+    @Subscribe
+    fun onTick(event: Tick){
+        mc.networkHandler!!.sendPacket( LookOnly(mc.player!!.yaw, 90.0f, mc.player!!.isOnGround))
+    }
+
+    @Subscribe
+    fun onRead(event: ReadPacket){
+        if (event.packet is LookOnly) {
+            event.isCancelled()
+        }
+        if(event.packet is Both){
+            var packet = event.packet as Both
+            mc.networkHandler!!.sendPacket(PositionOnly(packet.getX(mc.player!!.x), packet.getY(mc.player!!.y), packet.getZ(mc.player!!.z), mc.player!!.isOnGround))
+            event.isCancelled()
+        }
+    }
+    //TODO: add an item use event and for a moment use the correct rotation while an item is being used ie, ender pearl.
+}
