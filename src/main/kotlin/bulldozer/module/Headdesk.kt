4da4ab6e985c6125e1a5843bc352f895775ed8a0
Afel <prteza@gmail.com>
2020-10-01 11:54:11 -0700

    Added action override, and spinbot to headdesk module

--- src/main/kotlin/bulldozer/module/Headdesk.kt
+++ src/main/kotlin/bulldozer/module/Headdesk.kt
@@ -3,27 +3,35 @@ package bulldozer.module
 import bulldozer.Module
 import bulldozer.events.SendPacket
 import bulldozer.events.Tick
+import bulldozer.gui.SettingBoolean
 import com.google.common.eventbus.Subscribe
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket.*;
 
-class Headdesk: Module("Headdesk", emptyArray<Any>()) {
+class Headdesk: Module("Headdesk", arrayOf<Any>(SettingBoolean("Spinbot", false), SettingBoolean("DisableWhenClick", true))) {
     @Subscribe
     fun onTick(event: Tick){
         if(!toggled) return
-        mc.networkHandler!!.sendPacket( LookOnly(mc.player!!.yaw, 90.0f, mc.player!!.isOnGround))
+        val click = mc.options.keyAttack.isPressed || mc.options.keyUse.isPressed
+        val spin: Float = if((settings[0] as SettingBoolean).value) (Math.random() * 360 - 180).toFloat() else mc.player!!.yaw
+        if(!((settings[1] as SettingBoolean).value && click))
+            mc.networkHandler!!.sendPacket( LookOnly(spin, 90.0f, mc.player!!.isOnGround))
     }
 
     @Subscribe
     fun onSend(event: SendPacket){
         if(!toggled) return
-        if (event.packet is LookOnly) {
-            event.isCancelled()
-        }
-        if(event.packet is Both){
-            var packet = event.packet as Both
-            mc.networkHandler!!.sendPacket(PositionOnly(packet.getX(mc.player!!.x), packet.getY(mc.player!!.y), packet.getZ(mc.player!!.z), mc.player!!.isOnGround))
-            event.isCancelled()
+        val click = mc.options.keyAttack.isPressed || mc.options.keyUse.isPressed
+        if((settings[1] as SettingBoolean).value && click){
+            if (event.packet !is LookOnly) mc.networkHandler!!.sendPacket(LookOnly(mc.player!!.yaw,mc.player!!.pitch,mc.player!!.isOnGround))
+        } else {
+            if (event.packet is LookOnly) {
+                event.isCancelled()
+            }
+            if(event.packet is Both){
+                var packet = event.packet as Both
+                mc.networkHandler!!.sendPacket(PositionOnly(packet.getX(mc.player!!.x), packet.getY(mc.player!!.y), packet.getZ(mc.player!!.z), mc.player!!.isOnGround))
+                event.isCancelled()
+            }
         }
     }
-    //TODO: add an item use event and for a moment use the correct rotation while an item is being used ie, ender pearl.
 }
