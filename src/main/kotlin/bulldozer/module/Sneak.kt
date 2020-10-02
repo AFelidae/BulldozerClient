d07aa8e712d1c0f85b1c2e99257e366bac1d76e1
Afel <prteza@gmail.com>
2020-10-01 19:29:29 -0700

    Added jump feature to sneak so it doesn't get stuck on ledges
    (to deal with server anti cheat)

--- src/main/kotlin/bulldozer/module/Sneak.kt
+++ src/main/kotlin/bulldozer/module/Sneak.kt
@@ -2,11 +2,12 @@ package bulldozer.module
 
 import bulldozer.Module;
 import bulldozer.events.Tick
+import bulldozer.gui.SettingBoolean
 import bulldozer.gui.SettingMode
 import com.google.common.eventbus.Subscribe
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket
 
-class Sneak: Module("Sneak", arrayOf(SettingMode("Mode", arrayOf("Packet", "Input")))) {
+class Sneak: Module("Sneak", arrayOf(SettingMode("Mode", arrayOf("Packet", "Input")),SettingBoolean("LedgeHop", false))) {
     override fun onDisable() {
         mc.player!!.input.sneaking = false
         mc.player!!.isSneaking = false
@@ -22,6 +23,9 @@ class Sneak: Module("Sneak", arrayOf(SettingMode("Mode", arrayOf("Packet", "Inpu
     @Subscribe
     fun onTick(event: Tick) {
         if (toggled) {
+            if(mc.player!!.isOnGround && mc.world!!.getBlockState(mc.player!!.blockPos.down(1)).isAir && (settings[1] as SettingBoolean).value){
+                mc.player!!.jump()
+            }
             if ((settings[0] as SettingMode).value == 1) mc.player!!.input.sneaking = true
             if ((settings[0] as SettingMode).value == 0) mc.player!!.networkHandler.sendPacket(
                 ClientCommandC2SPacket(
