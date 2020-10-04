aa5029eee349bfcbcefd860e9977cdb34e0c9ee6
Afel <prteza@gmail.com>
2020-10-04 01:34:41 -0700

    Made step so much easier
    Probably should just do onenable and disable instead of everytick

--- src/main/kotlin/bulldozer/module/Step.kt
+++ src/main/kotlin/bulldozer/module/Step.kt
@@ -2,12 +2,14 @@ package bulldozer.module
 
 import bulldozer.Module
 import bulldozer.events.Tick
+import bulldozer.gui.SettingFloat
 import com.google.common.eventbus.Subscribe
 
-class Step: Module("Step", emptyArray<Any>()) {
+class Step: Module("Step", arrayOf<Any>(SettingFloat("Height",1f, 0.5f, 10f))) {
 
     @Subscribe
     fun onTick(event: Tick) {
-        if(toggled && mc.player!!.horizontalCollision) mc.player!!.updatePosition(mc.player!!.x, mc.player!!.y+1,mc.player!!.z)
+        if(!toggled) mc.player!!.stepHeight = (settings[0] as SettingFloat).value;
+        else mc.player!!.stepHeight = 0.5f;
     }
 }
