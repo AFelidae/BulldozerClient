0a5f21f008524597a8c4d400a030d4995119cfd5
Afel <prteza@gmail.com>
2020-10-04 13:46:23 -0700

    The typo also existed in normal step :/

--- src/main/kotlin/bulldozer/module/Step.kt
+++ src/main/kotlin/bulldozer/module/Step.kt
@@ -9,7 +9,7 @@ class Step: Module("Step", arrayOf<Any>(SettingFloat("Height",1f, 0.5f, 10f))) {
 
     @Subscribe
     fun onTick(event: Tick) {
-        if(!toggled) mc.player!!.stepHeight = (settings[0] as SettingFloat).value;
+        if(toggled) mc.player!!.stepHeight = (settings[0] as SettingFloat).value;
         else mc.player!!.stepHeight = 0.5f;
     }
 }
