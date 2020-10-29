d61d35bf00b7cb4b2cfe67dfaf6d8182247487e7
Afel <prteza@gmail.com>
2020-10-29 13:45:59 -0700

    Fixed chams

--- src/main/kotlin/bulldozer/module/Chams.kt
+++ src/main/kotlin/bulldozer/module/Chams.kt
@@ -7,20 +7,18 @@ import bulldozer.utils.Typer
 import net.minecraft.entity.Entity
 
 class Chams : Module("Chams", arrayOf(
-    SettingBoolean("Animal", false),
-    SettingBoolean("Monster", false),
     SettingBoolean("Player", true),
-    SettingBoolean("Teammate", true),
-    SettingBoolean("Friend", true),
-    SettingBoolean("Item", true),
     SettingBoolean("Other", false)), true){
 
 
     fun shouldSee(e: Entity) : Boolean{
         if(!toggled) return false
         when(Typer.entity(e)){
+            EntityType.USER -> {
+                if((settings[0] as SettingBoolean).value) return true
+            }
             EntityType.OTHER -> {
-                if((settings[6] as SettingBoolean).value) return true
+                if((settings[1] as SettingBoolean).value) return true
             }
         }
         return false
