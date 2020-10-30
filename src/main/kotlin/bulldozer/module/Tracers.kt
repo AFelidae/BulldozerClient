294720433b788e6962847e3d9ed940cd73e456b6
Afel <prteza@gmail.com>
2020-10-29 19:17:14 -0700

    Simplified tracers

--- src/main/kotlin/bulldozer/module/Tracers.kt
+++ src/main/kotlin/bulldozer/module/Tracers.kt
@@ -9,19 +9,15 @@ import com.google.common.eventbus.Subscribe
 import net.minecraft.entity.player.PlayerEntity
 
 class Tracers : Module("Tracers", arrayOf(
-    SettingBoolean("Player", true),
-    SettingBoolean("Other", false),
     SettingFloat("Opacity", 0.3f, 0f, 1f)), true){
 
     @Subscribe
     fun onRender(event: Render3D){
-        val opacity = (settings[2] as SettingFloat).value
+        val opacity = (settings[0] as SettingFloat).value
         var wide = 2.5f
         for(e in mc.world!!.entities){
             if(!e.isAttackable) continue
-            if(e.isInvisible && !((settings[3] as SettingBoolean).value)) continue
-            if(e is PlayerEntity && e != mc.player)
-                    if((settings[0] as SettingBoolean).value) DrawUtil.entityLine(e,1f, 0f, 0f, opacity, 1f)
+            if(e is PlayerEntity && e != mc.player) DrawUtil.entityLine(e,1f, 0f, 0f, opacity, 1f)
         }
     }
 }
