8f9ebab076e946bff734078fcc6daf4f786fdc93
Afel <prteza@gmail.com>
2020-10-29 20:56:51 -0700

    Fixed bug with mute

--- src/main/kotlin/bulldozer/module/Mute.kt
+++ src/main/kotlin/bulldozer/module/Mute.kt
@@ -3,6 +3,10 @@ package bulldozer.module
 import bulldozer.Module
 
 class Mute: Module("Mute", emptyArray(), true){
-    public var temporaryException: Boolean = false;
+    public var temporaryException: Boolean = false
     //ClientConnection.java
+    
+    override fun onEnable() {
+        temporaryException = false
+    }
 }
