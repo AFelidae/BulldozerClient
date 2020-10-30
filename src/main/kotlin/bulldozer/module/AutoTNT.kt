a336716d7cedb2c3e6806f00ce96db5ad3f9cc57
Afel <prteza@gmail.com>
2020-10-30 11:46:17 -0700

    Clean up

--- src/main/kotlin/bulldozer/module/AutoTNT.kt
+++ src/main/kotlin/bulldozer/module/AutoTNT.kt
@@ -6,49 +6,20 @@ import bulldozer.events.Tick
 import bulldozer.gui.SettingBoolean
 import bulldozer.utils.DrawUtil
 import bulldozer.utils.TNTUtils
+import bulldozer.utils.TNTUtils.nextMove
 import com.google.common.eventbus.Subscribe
 import net.minecraft.block.Blocks
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Vec3d
 import kotlin.math.abs
+import kotlin.math.floor
 
 class AutoTNT: Module("AutoTNT", arrayOf<Any>(SettingBoolean("Debugging mode", true), SettingBoolean("Movement", false)), true){
-    var bestW: Double? = null
-    var bestP: BlockPos? = null
-
     @Subscribe
     fun render(event: Render3D){
         if(!toggled) return
+        val state = nextMove((settings[0] as SettingBoolean).value)
 
-        if(mc.player!!.isOnGround) TNTUtils.blacklist[mc.player!!.blockPos.down(1)] = true
-        var bestWeight: Double? = null
-        var bestPos: BlockPos? = null
-        for(x in -4..4){
-            for(z in -4..4) {
-                val blockPos = mc.player!!.blockPos.down(1).north(x).east(z)
-                val b = mc.world!!.getBlockState(blockPos)
-                if(b.isAir) continue
-                if(!(b.isOf(Blocks.SAND) || b.isOf(Blocks.GRAVEL) || b.isOf(Blocks.RED_SAND))) continue
-                if(TNTUtils.blacklist[blockPos] != null){
-                    TNTUtils.drawFloor(blockPos, 1.0f, 0.0f, 0.0f, 0.3f)
-                    continue
-                }
-                val weight = TNTUtils.getWeight(blockPos) - abs(x) - abs(z)
-                if((settings[0] as SettingBoolean).value) TNTUtils.drawFloor(blockPos, 1.0f, 1.0f, 1.0f, 0.3f)
-                if(bestWeight == null){
-                    bestPos = blockPos
-                    bestWeight = weight
-                }else{
-                    if(bestWeight < weight){
-                        bestWeight = weight
-                        bestPos = blockPos
-                    }
-                }
-            }
-        }
-        if(bestWeight != null)
-            if((settings[0] as SettingBoolean).value) TNTUtils.drawFloor(bestPos!!, 0.0f, 0.0f, 1.0f, 0.3f)
-        bestW = bestWeight
-        bestP = bestPos
     }
 
 
@@ -56,14 +27,13 @@ class AutoTNT: Module("AutoTNT", arrayOf<Any>(SettingBoolean("Debugging mode", t
     fun onTick(event: Tick){
         if(!toggled) return
 
+
         if(!(settings[1] as SettingBoolean).value) return
-        if(bestW != null && mc.player!!.isOnGround){
-            TNTUtils.lookAt(bestP!!.x.toDouble() + 0.5, bestP!!.y.toDouble() + 0.5, bestP!!.z.toDouble() + 0.5)
-            mc.player!!.jump()
-        }
+        //Actual movement stuff would be done here
     }
 
     override fun onDisable() {
-        TNTUtils.blacklist = HashMap<BlockPos, Boolean>()
+        super.onDisable()
+        TNTUtils.blacklist = HashSet<BlockPos>()
     }
 }
--- src/main/kotlin/bulldozer/utils/TNTUtils.kt
+++ src/main/kotlin/bulldozer/utils/TNTUtils.kt
@@ -1,22 +1,37 @@
 package bulldozer.utils
 
+import bulldozer.gui.SettingBoolean
 import net.minecraft.block.Blocks
 import net.minecraft.client.MinecraftClient
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.MathHelper
+import kotlin.math.abs
 import kotlin.math.atan2
 import kotlin.math.sqrt
 
 
 object TNTUtils {
-    var blacklist = HashMap<BlockPos, Boolean>(); //I swear theres some data structure that you can just check if its in it but its not a hashmap
+    var blacklist = HashSet<BlockPos>();
 
     fun joinLobby(){
         Chat.sendMessage("/play tnt_tntrun")
     }
 
-    fun nearestPlayerDistance(blockPos: BlockPos) : Double? {
+
+    private fun donut(){
+        val mc :MinecraftClient = MinecraftClient.getInstance()
+        val c1 = BlockPos(mc.player!!.pos.x + 0.3, mc.player!!.pos.y-1, mc.player!!.pos.z + 0.3)
+        val c2 = BlockPos(mc.player!!.pos.x - 0.3, mc.player!!.pos.y-1, mc.player!!.pos.z + 0.3)
+        val c3 = BlockPos(mc.player!!.pos.x + 0.3, mc.player!!.pos.y-1, mc.player!!.pos.z - 0.3)
+        val c4 = BlockPos(mc.player!!.pos.x - 0.3, mc.player!!.pos.y-1, mc.player!!.pos.z - 0.3)
+        blacklist.add(c1)
+        blacklist.add(c2)
+        blacklist.add(c3)
+        blacklist.add(c4)
+    }
+
+    private fun nearestPlayerDistance(blockPos: BlockPos) : Double? {
         val mc :MinecraftClient = MinecraftClient.getInstance()
         var nearest: Double? = null
         for(e in mc.world!!.entities){
@@ -43,7 +58,7 @@ object TNTUtils {
             for(z in -4..4){
                 //Will count itself and have a value of 1 at minimum
                 val b = mc.world!!.getBlockState(blockPos.north(x).east(z))
-                if(blacklist[blockPos.north(x).east(z)] != null) continue
+                if(blacklist.contains(blockPos.north(x).east(z))) continue
                 if(b.isOf(Blocks.SAND) || b.isOf(Blocks.GRAVEL) || b.isOf(Blocks.RED_SAND)) weight++
             }
         }
@@ -57,16 +72,53 @@ object TNTUtils {
     fun lookAt(x: Double, y: Double, z: Double) {
         var mc = MinecraftClient.getInstance()
         val diffX: Double = x - mc.player!!.x
-        val diffY: Double = y - (mc.player!!.y + mc.player!!.getEyeHeight(mc.player!!.pose))
         val diffZ: Double = z - mc.player!!.z
-        val diffXZ = sqrt(diffX * diffX + diffZ * diffZ)
         val yaw = Math.toDegrees(atan2(diffZ, diffX)).toFloat() - 90f
-        val pitch = (-Math.toDegrees(atan2(diffY, diffXZ))).toFloat()
         mc.player!!.yaw += MathHelper.wrapDegrees(yaw - mc.player!!.yaw)
-        mc.player!!.pitch += MathHelper.wrapDegrees(pitch - mc.player!!.pitch)
     }
 
-    fun drawFloor(blockPos: BlockPos, r: Float, g: Float, b: Float, a: Float){
+    private fun drawFloor(blockPos: BlockPos, r: Float, g: Float, b: Float, a: Float){
         DrawUtil.drawBox(blockPos.x.toDouble(), blockPos.y.toDouble() + 1, blockPos.z.toDouble(), blockPos.x.toDouble() +1, blockPos.y.toDouble() + 1, blockPos.z.toDouble() + 1, r, g, b, a)
+    }
+
+    fun nextMove(visuals: Boolean): BlockPos?{
+        var mc = MinecraftClient.getInstance()
+
+        if(mc.player!!.isOnGround) {
+            if(mc.world!!.getBlockState(mc.player!!.blockPos.down(1)).isAir){
+                donut()
+            }else{
+                TNTUtils.blacklist.add(mc.player!!.blockPos.down(1))
+            }
+        }
+
+        var bestWeight: Double? = null
+        var bestPos: BlockPos? = null
+        for(x in -4..4){
+            for(z in -4..4) {
+                val blockPos = mc.player!!.blockPos.down(1).north(x).east(z)
+                val b = mc.world!!.getBlockState(blockPos)
+                if(!(b.isOf(Blocks.SAND) || b.isOf(Blocks.GRAVEL) || b.isOf(Blocks.RED_SAND))) continue
+                if(TNTUtils.blacklist.contains(blockPos) && visuals){
+                    TNTUtils.drawFloor(blockPos, 1.0f, 0.0f, 0.0f, 0.3f)
+                    continue
+                }
+                val weight = TNTUtils.getWeight(blockPos) - abs(x) - abs(z)
+                if(visuals) TNTUtils.drawFloor(blockPos, 1.0f, 1.0f, 1.0f, 0.3f)
+                if(x==0&&z==0) continue
+                if(bestWeight == null){
+                    bestPos = blockPos
+                    bestWeight = weight
+                }else{
+                    if(bestWeight < weight){
+                        bestWeight = weight
+                        bestPos = blockPos
+                    }
+                }
+            }
+        }
+        if(bestWeight != null)
+            if(visuals) TNTUtils.drawFloor(bestPos!!, 0.0f, 0.0f, 1.0f, 0.3f)
+        return bestPos
     }
 }
